07c4feb451d82d652d556ed9d4b56080452a13a3
rakibulislam8226 <rakibulislam8226@gmail.com>
2023-06-07 19:03:16 +0600

    [ADDED] Automated API documentation swagger added

--- src/config/urls.py
+++ src/config/urls.py
@@ -16,10 +16,27 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Coding Test API",
+      default_version='v1',
+      description="Coding Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="rakibulislam8226@gmail.com"),
+      license=openapi.License(name="Testing License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('corporate_assets.urls')),
 
     path('api-auth/', include('rest_framework.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
